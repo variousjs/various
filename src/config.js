af4c2871773366d892c2a3b1fd56788a9376af40
LoeiFy <LoeiFy@gmail.com>
2019-12-16 21:39:01 +0800

    chore: packages version
    Change-Id: Ifc2e52e43f78e95a0dfe2883b3c2282bb46c3f26

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const getScriptSrc = () => document.currentScript.src
 
 export const DEFAULT_PACKAGES = {
-  react: 'https://unpkg.com/react@16.12.0/umd/react.production.min.js',
-  'react-dom': 'https://unpkg.com/react-dom@16.12.0/umd/react-dom.production.min.js',
-  'react-router-dom': 'https://unpkg.com/react-router-dom@5.1.2/umd/react-router-dom.min.js',
-  'nycticorax': 'https://unpkg.com/nycticorax@1.1.0/lib/index.js',
+  react: 'https://unpkg.com/react/umd/react.production.min.js',
+  'react-dom': 'https://unpkg.com/react-dom/umd/react-dom.production.min.js',
+  'react-router-dom': 'https://unpkg.com/react-router-dom/umd/react-router-dom.min.js',
+  'nycticorax': 'https://unpkg.com/nycticorax/lib/index.js',
 }
 
 export const SCRIPT_SRC = getScriptSrc()
