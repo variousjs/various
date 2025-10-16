8155a5ce15d8fae107598d44fc789c25fd1a94ef
loeify <loeify@gmail.com>
2025-10-16 18:30:20 +0800

    feat: i18n vue test

---
+++ test/components/i18n/i.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { VueVarious, I18n } from '@variousjs/various'
+import zh from './zh.json'
+import en from './en.json'
+
+const V = defineComponent({
+  props: {
+    various: Object as PropType<VueVarious>,
+  },
+
+  setup(props) {
+    return {
+      t: props.various?.$t || (() => null)
+    }
+  }
+})
+
+V.$i18n = (() => {
+  return { localeKey: 'locale', resources: { zh, en } }
+}) as I18n
+
+export default V
+</script>
+
+<template>
+  <h3>Vue Component</h3>
+  <div class="value">
+    <p>name: {{ t('name') }}</p>
+    <p>greet: {{ t('greet', { name: 'C', name2: 'D' }) }}</p>
+  </div>
+</template>
